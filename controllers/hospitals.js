612bafb53e71afc0c9486f5026594461c862ee0a
Auuming <auming.itkit@gmail.com>
2025-11-20 23:32:12 +0700

    fork from assignment

---
+++ controllers/hospitals.js
@@ -0,0 +1,150 @@
+const Hospital = require('../models/Hospital');
+const Appointment = require('../models/Appointment');
+
+function transformQuery(input) {
+  const output = {};
+  for (const key in input) {
+    const match = key.match(/^(\w+)\[(\w+)\]$/);
+    if (match) {
+      const field = match[1];
+      const operator = match[2];
+      output[field] = { [`$${operator}`]: input[key] };
+    } else {
+      output[key] = input[key];
+    }
+  }
+  return output;
+}
+
+//@desc Get all hospitals
+//@route GET /api/v1/hospitals
+//@access Public
+exports.getHospitals = async(req,res,next) => {
+    let query;
+
+    //Copy req.query
+    const reqQuery= {...req.query};
+
+    //Fields to exclude
+    const removeFields=['select', 'sort', 'page', 'limit'];
+
+    //Loop over remove fields and deletе them from reqQuery
+    removeFields.forEach(param=>delete reqQuery[param]);
+
+    //Create query string
+    let queryStr = JSON.stringify(req.query);
+
+    // queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+    queryStr = queryStr.replace(/"(\w+)\[(\w+)\]":\s*"([^"]+)"/g, (_, field, op, value) => `"${field}":{"$${op}":"${value}"}`);
+
+    //finding resource
+    query = Hospital.find(JSON.parse (queryStr)).populate('appointments');
+
+    //Select Fields
+    if(req.query.select) {
+        const fields = req.query.select.split(',').join(' ');
+        query = query.select(fields);
+    }
+
+    //Sort
+    if(req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+    } else {
+        query = query.sort('-createdAt');
+    }
+
+    //Pagination
+    const page = parseInt(req.query.page,10) || 1;
+    const limit = parseInt(req.query.limit,10) || 25;
+    const startIndex = (page-1)*limit;
+    const endIndex = page*limit;
+    const total = await Hospital.countDocuments();
+    query = query.skip(startIndex).limit(limit);
+
+    try {
+        //Executing query
+        const hospitals = await query
+
+        //Pagination result
+        const pagination = {};
+        if (endIndex < total) {
+            pagination.next = {
+                page: page+1,
+                limit
+            }
+        }
+        if (startIndex > 0) {
+            pagination.prev = {
+                page: page-1,
+                limit
+            }
+        }
+
+        //Success response
+        res.status(200).json({success:true, count:hospitals.length, pagination, data:hospitals});
+    } catch(err) {
+        res.status(400).json({success:false, error:err});
+    }
+};
+
+//@desc Get single hospital
+//@route GET /api/v1/hospitals/:id
+//@access Public
+exports.getHospital = async(req,res,next) => {
+    try {
+        const hospital = await Hospital.findById(req.params.id);
+        if (!hospital) {
+            return res.status(400).json({success:false, error:"not exist"});
+        }
+        res.status(200).json({success:true, data:hospital});
+    } catch(err) {
+        res.status(400).json({success:false, error:err});
+    }
+};
+
+//@desc Create new hospital
+//@route POST /api/v1/hospitals
+//@access Private
+exports.createHospital = async(req,res,next) => {
+    const hospital = await Hospital.create(req.body);
+    res.status(201).json({
+        success: true,
+        data: hospital
+    });
+};
+
+//@desc Update hospital
+//@route PUT /api/v1/hospitals/:id
+//@access Private
+exports.updateHospital = async(req,res,next) => {
+    try {
+        const hospital = await Hospital.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        if (!hospital) {
+            return res.status(400).json({success:false, error:"not exist"});
+        }
+        res.status(200).json({success:true, data:hospital});
+    } catch(err) {
+        res.status(400).json({success:false, error:err});
+    }
+};
+
+//@desc Delete hospital
+//@route DELETE /ap1/v1/hospitals/:id
+//@access Private
+exports.deleteHospital = async(req,res,next) => {
+    try {
+        const hospital = await Hospital.findById(req.params.id);
+        if (!hospital) {
+            return res.status(400).json({success:false, message:`Hospital not found with id of ${req.params.id}`});
+        }
+        await Appointment.deleteMany({ hospital: req.params.id });
+        await Hospital.deleteOne({_id:req.params.id});
+        res.status(200).json({success:true, data:{}});
+    } catch(err) {
+        res.status(400).json({success:false, error:err});
+    }
+};
